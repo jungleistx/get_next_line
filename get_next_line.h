f3ebb8ab59e9c787f7b7a0ded78850767708715d
Roope Vuorenlehto <rvuorenl@c3r5p1.hive.fi>
2021-12-17 13:35:12 +0200

    commit all, including libft and contents

---
+++ get_next_line.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rvuorenl <rvuorenl@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/03 13:13:27 by rvuorenl          #+#    #+#             */
+/*   Updated: 2021/12/15 13:28:13 by rvuorenl         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include "libft/includes/libft.h"
+# define BUFF_SIZE 8
+
+int	get_next_line(const int fd, char **line);
+
+#endif
